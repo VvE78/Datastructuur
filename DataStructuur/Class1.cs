bc77602f9ca23d9a7d6aa1a5bcdedbd7cf2bcc6e
VvE <vincentvanempel@gmail.com>
2017-05-13 21:02:04 +0000

    initiele commit. Basis classes gemaakt.

---
+++ DataStructuur/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuur
+{
+    
+    public class Systeem
+    {
+        private List<Item> list;
+
+        public Systeem()
+        {
+            list = new List<Item>();
+        }
+
+        public Item AddItem(Item item)
+        {
+            Items.Add(item);
+            return item;
+        }
+
+        public List<Item> Items { get { return list; } }
+
+        public override string ToString()
+        {
+            string output = "Systeem bevat " + Items.Count.ToString() + " items";
+            foreach (Item item in Items)
+            {
+                output += Environment.NewLine + item.ToString();                
+            }
+            return output;
+        }
+
+        public List<Item> ToplevelItems()
+        {
+            var TLI = from i in Items where i.Reason == null select i;
+            var Result = new List<Item>();
+            foreach (Item i in TLI)
+                Result.Add(i);
+            return Result;            
+        }
+
+//        public List<Item> LowlevelItems()
+        //{ 
+        //  //  var LLI = from i in Items where i.reas
+        //}
+
+        public List<Item> NextActions()
+        {
+            var NA = from i in Items where i.IsActionable == true select i;
+            var Result = new List<Item>();
+            foreach (Item i in NA)
+                Result.Add(i);
+            return Result;
+        }
+    }
+
+    public class Item
+    {
+        public string Titel;
+
+        public Item Reason;
+
+        public bool IsActionable;
+
+        public override string ToString()
+        {
+            if (Reason == null)
+                return Titel + "; Reason = leeg" + "; IsActionable = " + IsActionable.ToString();
+            else
+                return Titel + "; Reason = " + Reason.Titel + "; IsActionable = " + IsActionable.ToString();
+        }
+    }
+}
---
+++ DataStructuurTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using DataStructuur;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuurTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Systeem s;
+        private Item taak;
+        private Item reden;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            s = new Systeem();
+
+            taak = new Item();
+            taak.Titel = "bel makelaar";
+            taak.IsActionable = true;
+
+            reden = new Item();
+            reden.Titel = "huis verkopen";
+
+            taak.Reason = reden;
+
+            s.AddItem(taak);
+            s.AddItem(reden);
+        }
+
+        [TestMethod]
+        public void Test_BasisToevoegenVanTakenAanSysteem()
+        {
+            Console.Write(s.ToString());
+
+            Assert.AreEqual<int>(2, s.Items.Count, "Verwachten nu 2 items");        
+        }
+
+        [TestMethod]
+        public void Test_TopLevelItems()
+        {
+            Assert.AreEqual<int>(1, s.ToplevelItems().Count, "Verwacht 1 tli");
+            Console.Write(s.ToplevelItems()[0].ToString());
+        }
+
+        [TestMethod]
+        public void Test_NextActions()
+        {
+            Assert.AreEqual<int>(1, s.NextActions().Count, "Verwacht 1 next action");
+            Console.Write(s.NextActions()[0].ToString());
+        }
+    }
+}
